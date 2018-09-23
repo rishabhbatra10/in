0e4d45f298dc7473777515ffdf3c3ddcd0b0aa98
Rishabh Batra <rishabh.batra@klassifai.com>
2018-09-23 15:19:54 +0530

    added districts of india

--- states.py
+++ states.py
@@ -9,7 +9,7 @@ state_file = './data/states.csv'
 
 class State(object):
 
-    def __init__(self, abbr, name):
+    def __init__(self, abbr: str, name: str):
         self.name = name
         self.abbr = abbr
 
@@ -22,11 +22,11 @@ class State(object):
 
 def load_states():
     """
-    Load state data from a txt file in the package
+    Load state data from a csv file in the package
 
-    creates list of states, union territories.
+    Creates list of states.
     Also adds state abbrevation attribute access to the package.
-    in.states.MD
+    # in.states.AR
     """
 
     with open(state_file) as statesfile:
@@ -44,6 +44,31 @@ def load_states():
             globals()[state.abbr] = state
 
 
+def load_ut():
+    """
+    Load union territories from a csv file provided with the package.
+
+    Creates a list of Union Teriitories.
+    Also add abbrevation attribute access to the package
+    # in.states.PY
+
+    """
+    with open(ut_file) as utsfile:
+        uts = csv.reader(utsfile, delimiter= ',')
+        for row in uts:
+            ut = State(row[1], row[0])
+
+            # creates a list of uts
+            UNION_TERRITORIES.append(ut)
+
+            # appends to the master list of all states and territories
+            STATES_AND_TERRITORIES.append(ut)
+
+            #provides package-level abbrevation
+            globals()[ut.abbr] = ut
+
+
 # initialising states and territories
 if __name__ == '__main__':
     load_states()
+    load_ut()
