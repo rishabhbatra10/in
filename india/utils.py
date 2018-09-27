b8f659494eed47b37b4cec8749cd8fa482f0d37a
Rishabh Batra <rishabh.batra@klassifai.com>
2018-09-27 16:02:40 +0530

    modified data files

--- india/utils.py
+++ india/utils.py
@@ -5,6 +5,6 @@ WORKING_DIRECTORY = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], st
                             0].rstrip().decode('utf-8') + '/'
 
 FILE_NAME = {
-    'states': WORKING_DIRECTORY + 'india/data/states.csv',
-    'ut': WORKING_DIRECTORY + 'india/data/ut.csv'
+    'states': WORKING_DIRECTORY + 'data/states.csv',
+    'ut': WORKING_DIRECTORY + 'data/ut.csv'
 }
