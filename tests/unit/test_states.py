9f9506effd9648ae14715f691339f1e41379aef6
Rishabh Batra <ribhu.1996@gmail.com>
2019-11-04 16:59:00 +0530

    Added test for states class

---
+++ tests/unit/test_states.py
@@ -0,0 +1,43 @@
+""" Testing indian states class """
+# @coding: utf-8
+# @author: Rishabh Batra
+# @email: rishabhbatra10@gmail.com
+# TODO: Write tests for each an every function
+# TODO: Add test failure messages
+
+# external imports
+import unittest
+
+# module imports
+from india import states
+
+
+class TestStates(unittest.TestCase):
+    """ Test suite for States module """
+    
+    def test_state_initialization(self):
+        """ Testing state initialization """
+        abbrevation = 'KA'
+        name = 'Karnataka'
+        capital = 'Bangalore'
+        population = '61095297' # in numbers
+        area = '191791' # km^2 
+        language = 'Kannada'
+
+        state = states.State(abbr=abbrevation, 
+                             name=name, 
+                             capital=capital, 
+                             population=population, 
+                             area=area, 
+                             lang=language)
+        
+        self.assertEqual(str(state), name)
+        self.assertEqual(type(state), '<State: %s>'.format(name))
+        self.assertEqual(state.name, name)
+        self.assertEqual(state.abbr, abbr)
+        self.assertEqual(state.capital, capital)
+        self.assertEqual(state.population, int(population))
+        self.assertEqual(state.area, int(area))
+        self.assertEqual(state.density, int(population)/ int(area))
+        self.assertEqual(state.language, language)
+
