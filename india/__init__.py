879bc0a24c44532f79c360bf724f352c29b5e5bb
Rishabh Batra <ribhu.1996@gmail.com>
2019-11-10 20:49:05 +0530

    Recommitted Deleted __init__ file

---
+++ india/__init__.py
@@ -0,0 +1,12 @@
+"""Initializing module India """
+# @coding: utf-8
+# @author: Rishabh Batra
+# @email: rishabhbatra1002@gmail.com
+
+# Module imports
+from . import states
+from .states import STATES, UNION_TERRITORIES, STATES_AND_TERRITORIES
+from .cities import CITIES
+
+__appname__ = 'pyindia'
+__version__ = '1.1.0'
